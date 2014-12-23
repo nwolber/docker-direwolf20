f2ab0a87aaac1ca6e5736870e980121bf4a1f38e
Niklas Wolber <niklas.wolber@octopost.eu>
2014-12-23 16:28:16 +0100

    adapt server jar name

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,4 +6,4 @@ if [ ! -f minecraft_server.1.7.10.jar ]; then
     ./install.sh
 fi
 echo "make sure to read eula.txt before playing!"
-java -server -Xms2048m -Xmx3072m -XX:PermSize=256m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -jar FTBServer-1.7.10-1231.jar nogui
+java -server -Xms2048m -Xmx3072m -XX:PermSize=256m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -jar FTBServer-1.7.10-1272.jar nogui
